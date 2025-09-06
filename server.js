54e4dc11ee0141ff76f5d8b7c7e959bcd76b84d5
Amanda Loch <amandaloch.studio@gmail.com>
2025-09-06 17:38:11 -0300

    configurando server.js para servir public/index.html

--- server.js
+++ server.js
@@ -2,18 +2,16 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
-// Porta dinâmica para Render, ou 3000 localmente
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos da pasta "public"
+// Servir arquivos estáticos da pasta public
 app.use(express.static(path.join(__dirname, "public")));
 
-// Rota principal
+// Rota padrão -> abrir index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
